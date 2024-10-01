92d2af004e13ee8a680e68285a7de2feb20434c6
Antonio Dias <addsjunior@gmail.com>
2024-10-01 18:10:13 -0300

    Commiting changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,308 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inputValue, setInputValue] = useState("");
+  const [patternValue, setPatternValue] = useState("");
+  const [selectedFromBase, setSelectedFromBase] = useState("2");
+  const [selectedToBase, setSelectedToBase] = useState("");
+  const [result, setResult] = useState("");
+  const [isExpanded, setIsExpanded] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleFromBaseChange = (e) => {
+    setSelectedFromBase(e.target.value);
+    setInputValue("");
+    setResult("");
+
+    switch (e.target.value) {
+      case "8":
+        setPatternValue("[0-7]*");
+        break;
+      case "10":
+        setPatternValue("[0-9]*");
+        break;
+      case "16":
+        setPatternValue("[0-9A-F]*");
+        break;
+      default:
+        setPatternValue("[01]*");
+        break;
+    }
+  };
+
+  const handleToBaseChange = (e) => {
+    setSelectedToBase(e.target.value);
+
+    if (inputValue === "") return;
+
+    calculateResult(
+      parseInt(inputValue, parseInt(selectedFromBase)),
+      e.target.value
+    );
+  };
+
+  const calculateResult = (value: number, base: string) => {
+    if (isNaN(value)) {
+      setResult("");
+      return;
+    }
+
+    if (base == "2") {
+      let formattedBinary = value.toString(2);
+      let mustPad = formattedBinary.length % 4 !== 0;
+      let division = Math.floor(formattedBinary.length / 4);
+      let pad = mustPad ? (division + 1) * 4 : formattedBinary.length;
+      console.log("pad is", pad, "length is ", formattedBinary.length);
+      formattedBinary = formattedBinary
+        .padStart(pad, "0")
+        .replace(/(.{4})/g, "$1 ")
+        .trim();
+      setResult(formattedBinary);
+    } else {
+      setResult(value.toString(parseInt(base)));
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    let mask;
+
+    switch (selectedFromBase) {
+      case "8":
+        mask = /[^0-7]/g;
+        break;
+      case "10":
+        mask = /[^0-9]/g;
+        break;
+      case "16":
+        mask = /[^0-9A-F]/gi;
+        break;
+      default:
+        mask = /[^01]/g;
+    }
+    const filteredValue = inputValue.replace(mask, "");
+    setInputValue(filteredValue);
+    if (selectedToBase != "") {
+      calculateResult(
+        parseInt(filteredValue, parseInt(selectedFromBase)),
+        selectedToBase
+      );
+    }
+    // setSelectedToBase("");
+    // setResult("");
+  };
+
+  const handleConvert = () => {
+    console.log(`Valor: ${inputValue}, Base: ${selectedFromBase}`);
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4'>
+      <h1 className='text-2xl font-bold mb-4 font-sans'>Numerical Converter</h1>
+
+      <div className='w-full max-w-lg py-2 font-semibold font-sans'>
+        Convert from
+      </div>
+
+      <div
+        id='from'
+        className='grid grid-cols-4 gap-2 justify-items-stretch w-full max-w-lg pb-4 text-sm font-sans'
+      >
+        <label className='cursor-pointer'>
+          <input
+            type='radio'
+            name='fromBase'
+            value='2'
+            className='hidden peer'
+            checked={selectedFromBase === "2"}
+            onChange={handleFromBaseChange}
+          />
+          <div
+            className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <span className='block sm:hidden'>Bin</span>
+            <span className='hidden sm:block'>Binary</span>
+          </div>
+        </label>
+
+        <label className='cursor-pointer'>
+          <input
+            type='radio'
+            name='fromBase'
+            value='8'
+            className='hidden peer'
+            checked={selectedFromBase === "8"}
+            onChange={handleFromBaseChange}
+          />
+          <div
+            className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+          >
+            <span className='block sm:hidden'>Oct</span>
+            <span className='hidden sm:block'>Octal</span>
+          </div>
+        </label>
+
+        <label className='cursor-pointer'>
+          <input
+            type='radio'
+            name='fromBase'
+            value='10'
+            className='hidden peer'
+            checked={selectedFromBase === "10"}
+            onChange={handleFromBaseChange}
+          />
+          <div
+            className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+          >
+            <span className='block sm:hidden'>Dec</span>
+            <span className='hidden sm:block'>Decimal</span>
+          </div>
+        </label>
+
+        <label className='cursor-pointer'>
+          <input
+            type='radio'
+            name='fromBase'
+            value='16'
+            className='hidden peer'
+            checked={selectedFromBase === "16"}
+            onChange={handleFromBaseChange}
+          />
+          <div
+            className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+          >
+            <span className='block sm:hidden'>Hex</span>
+            <span className='hidden sm:block'>Hexadecimal</span>
+          </div>
+        </label>
+      </div>
+
+      <input
+        type='text'
+        value={inputValue}
+        pattern={patternValue}
+        onChange={handleInputChange}
+        placeholder='Insert the number'
+        className='w-full max-w-lg p-4 my-2 border focus:ring-2 ring-sky-400 rounded-lg text-lg font-mono'
+      />
+
+      <div className='w-full max-w-lg py-2 font-semibold font-sans'>
+        Convert to
+      </div>
+
+      <div
+        id='from'
+        className='grid grid-cols-3 gap-2 w-full justify-items-stretch max-w-lg pb-4 p-2 text-sm font-sans'
+      >
+        {selectedFromBase !== "2" && (
+          <label className='cursor-pointer'>
+            <input
+              type='radio'
+              name='toBase'
+              value='2'
+              className='hidden peer'
+              checked={selectedToBase === "2"}
+              onChange={handleToBaseChange}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+              className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+            >
+              <span className='block sm:hidden'>Bin</span>
+              <span className='hidden sm:block'>Binary</span>
+            </div>
+          </label>
+        )}
+
+        {selectedFromBase !== "8" && (
+          <label className='cursor-pointer'>
+            <input
+              type='radio'
+              name='toBase'
+              value='8'
+              className='hidden peer'
+              checked={selectedToBase === "8"}
+              onChange={handleToBaseChange}
+            />
+            <div
+              className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+            >
+              <span className='block sm:hidden'>Oct</span>
+              <span className='hidden sm:block'>Octal</span>
+            </div>
+          </label>
+        )}
+
+        {selectedFromBase !== "10" && (
+          <label className='cursor-pointer'>
+            <input
+              type='radio'
+              name='toBase'
+              value='10'
+              className='hidden peer'
+              checked={selectedToBase === "10"}
+              onChange={handleToBaseChange}
+            />
+            <div
+              className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+            >
+              <span className='block sm:hidden'>Dec</span>
+              <span className='hidden sm:block'>Decimal</span>
+            </div>
+          </label>
+        )}
+
+        {selectedFromBase !== "16" && (
+          <label className='cursor-pointer'>
+            <input
+              type='radio'
+              name='toBase'
+              value='16'
+              className='hidden peer'
+              checked={selectedToBase === "16"}
+              onChange={handleToBaseChange}
+            />
+            <div
+              className='bg-gray-200 peer-checked:bg-sky-500 peer-hover:bg-sky-400 peer-hover:text-white text-gray-800 
+            peer-checked:text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out text-center'
+            >
+              <span className='block sm:hidden'>Hex</span>
+              <span className='hidden sm:block'>Hexadecimal</span>
+            </div>
+          </label>
+        )}
+      </div>
+
+      {result !== "" && (
+        <div className='w-full max-w-lg'>
+          <div className='bg-sky-100 text-sky-700 text-3xl text-center p-4 mb-2 rounded-md font-mono'>
+            {result}
+          </div>
+          <div className='p-2 text-sm bg-sky-400 text-sky-100 rounded-md'>
+            <button
+              onClick={() => setIsExpanded(!isExpanded)}
+              className='text-sky-100 p-1 rounded-lg hover:text-sky-200 focus:outline-none'
+            >
+              Explain
+            </button>
+
+            <div
+              className={`transition-all duration-200 ease-in-out overflow-hidden ${
+                isExpanded ? "max-h-96" : "max-h-0"
+              } mt-2 bg-gray-200 text-gray-700 p-2`}
+            >
+              To be implemented.
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
